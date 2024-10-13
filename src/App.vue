89447f78a74b5dd31210ae8a8d329de8344a3f06
Hashara13 <hnethmin13@gmail.com>
2024-10-13 18:55:33 +0530

    from binding II

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import {ref} from 'vue'
 const text=ref('')
 function Ontype(e){
@@ -8,4 +8,15 @@ function Ontype(e){
 <template>
   <input :value="text" @input="Ontype"  type="text">
   <h1>{{ text }}</h1>
+</template> -->
+
+<script setup>
+import {ref} from 'vue'
+const text=ref('')
+
+</script>
+<template>
+  <input v-model="text" type="text">
+  <h1>{{ text }}</h1>
 </template>
+
